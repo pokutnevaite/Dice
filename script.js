a33cd01c981090d10d64a556a92914ff50fef08a
pokutnevaite <62509636+pokutnevaite@users.noreply.github.com>
2020-10-25 21:09:13 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -5,9 +5,11 @@ var randomNumber2 = Math.floor((Math.random() * 6) + 1);
 document.querySelector(".img2").setAttribute("src","images/dice" + randomNumber2 + ".png");
 
 if (randomNumber1 < randomNumber2) {
-    document.querySelector("h1").innerHTML = "Player 2 Wins 🚩";
+    document.querySelector("h1").innerHTML = "Player 2 Wins";
+    document.querySelector(".p2").innerHTML = "Player 2🚩";
 } else if (randomNumber1 > randomNumber2) {
-    document.querySelector("h1").innerHTML = "🚩 Player 1 Wins";
+    document.querySelector("h1").innerHTML = "Player 1 Wins";
+    document.querySelector(".p2").innerHTML = "🚩Player 1";
 } else {
     document.querySelector("h1").innerHTML = "Draw!";
 }
