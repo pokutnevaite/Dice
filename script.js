4286dc6c364ea00d18bd17d8a63b67458e76e7d8
pokutnevaite <62509636+pokutnevaite@users.noreply.github.com>
2020-10-26 13:05:35 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
-var randomNumber1 =  Math.floor((Math.random() * 6) + 1);
-document.querySelector(".img1").setAttribute("src", "images/dice" + randomNumber1 + ".png");
+document.querySelector(".btn").addEventListener("click", function () {
+    var randomNumber1 = Math.floor(Math.random() * 6 + 1);
+    document
+        .querySelector(".img1")
+        .setAttribute("src", "images/dice" + randomNumber1 + ".png");
 
-var randomNumber2 = Math.floor((Math.random() * 6) + 1);
-document.querySelector(".img2").setAttribute("src","images/dice" + randomNumber2 + ".png");
+    var randomNumber2 = Math.floor(Math.random() * 6 + 1);
+    document
+        .querySelector(".img2")
+        .setAttribute("src", "images/dice" + randomNumber2 + ".png");
 
-if (randomNumber1 < randomNumber2) {
-    document.querySelector("h1").innerHTML = "Player 2 Wins!";
-    document.querySelector(".p2").innerHTML = "Player 2🚩";
-} else if (randomNumber1 > randomNumber2) {
-    document.querySelector("h1").innerHTML = "Player 1 Wins!";
-    document.querySelector(".p1").innerHTML = "🚩Player 1";
-} else {
-    document.querySelector("h1").innerHTML = "Draw!";
-}
-
-function refreshPage(){
-    window.location.reload();
-} 
+    if (randomNumber1 < randomNumber2) {
+        document.querySelector("h1").innerHTML = "Player 2 Wins!";
+        document.querySelector(".p2").innerHTML = "Player 2🚩"; 
+        document.querySelector(".p1").innerHTML = "Player 1";
+    } else if (randomNumber1 > randomNumber2) {
+        document.querySelector("h1").innerHTML = "Player 1 Wins!";
+        document.querySelector(".p1").innerHTML = "🚩Player 1";
+        document.querySelector(".p2").innerHTML = "Player 2";
+    } else {
+        document.querySelector("h1").innerHTML = "Draw!";
+        document.querySelector(".p1").innerHTML = "Player 1";
+        document.querySelector(".p2").innerHTML = "Player 2";
+    }
+});
