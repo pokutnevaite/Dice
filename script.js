85e09ce69d384ea389670cee9d24acbf93916039
pokutnevaite <62509636+pokutnevaite@users.noreply.github.com>
2020-10-26 15:27:36 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -9,17 +9,30 @@ document.querySelector(".btn").addEventListener("click", function () {
         .querySelector(".img2")
         .setAttribute("src", "images/dice" + randomNumber2 + ".png");
 
+        var player1Name = "Player 1";
+        var player2Name = "Player 2";
+        var win = " Wins!";
+        var draw = "Draw!";
+        var winFlag = "🚩";
+        var result = "";
+        var player1Text = "";
+        var player2Text = "";
+
     if (randomNumber1 < randomNumber2) {
-        document.querySelector("h1").innerHTML = "Player 2 Wins!";
-        document.querySelector(".p2").innerHTML = "Player 2🚩"; 
-        document.querySelector(".p1").innerHTML = "Player 1";
+        result = player2Name + win;
+        player2Text = player2Name + winFlag; 
+        player1Text = player1Name;
     } else if (randomNumber1 > randomNumber2) {
-        document.querySelector("h1").innerHTML = "Player 1 Wins!";
-        document.querySelector(".p1").innerHTML = "🚩Player 1";
-        document.querySelector(".p2").innerHTML = "Player 2";
+        result = player1Name + win;
+        player1Text = winFlag + player1Name;
+        player2Text = player2Name;
     } else {
-        document.querySelector("h1").innerHTML = "Draw!";
-        document.querySelector(".p1").innerHTML = "Player 1";
-        document.querySelector(".p2").innerHTML = "Player 2";
+        result = draw;
+        player1Text = player1Name;
+        player2Text = player2Name;
     }
+
+    document.querySelector("h1").innerHTML = result;
+    document.querySelector(".p1").innerHTML = player1Text;
+        document.querySelector(".p2").innerHTML = player2Text;
 });
